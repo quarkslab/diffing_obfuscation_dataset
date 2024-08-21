56f8eddaf9c7475e74e0b73c06673bb6bea5dccd
Roxane Cohen <rcohen@quarkslab.com>
2024-08-21 09:51:53 +0200

    update src

--- src/obfu_dataset/dataset.py
+++ src/obfu_dataset/dataset.py
@@ -1,54 +1,116 @@
 from pathlib import Path
 from typing import Iterable
+from zipfile import ZipFile
+import logging
+import requests
+import json
+import hashlib
 
 from obfu_dataset.types import Project, Obfuscator, ObPass, Sample, \
                                Compiler, Architecture, OptimLevel, BinaryType, \
                                SEED_NUMBER
 
-'''
-root/
-    project/
-        sources/
-        obfuscated/
-            obfuscator/
-                obfu-pass/
-                    level/
-
-'''
 
 
 class ObfuDataset(object):
     def __init__(self, root_dir: str|Path, json_links: Path|None=None):
         pass
         self.root_path = root_dir
-        # TODO initialize dataset
+        if json_links:
+            self.json_links = json_links
+        else:
+            default_path = Path('precompiled/links.json')
+            if default_path.exists():
+                self.json_links = default_path
+            else:
+                logging.ERROR("Missing link .json file in the current directory. Please provide one")
+                exit()
+        self._init_dirs()
 
 
     def _init_dirs(self):
-        # TODO: Create base structure
-        pass
+        ollvm_pass = [ObPass.CFF, ObPass.OPAQUE, ObPass.ENCODEARITH, ObPass.CFF_ENCODEARITH_OPAQUE]
+        
+        root = Path(self.root_path)
+        for proj in Project:
+            proj_dir = root / proj.value 
+            
+            src_dir = proj_dir / "sources"
+            src_dir.mkdir(parents=True, exist_ok=True)
+            
+            obf_dir = proj_dir / "obfuscated"
+            
+            for obfu in Obfuscator:
+                obfu_dir = obf_dir / obfu.value
+                
+                if obfu.value == 'tigress':
+                    for ob_pass in ObPass:
+                        ob_pass_dir = obfu_dir / ob_pass.value
+                        ob_pass_dir.mkdir(parents=True, exist_ok=True)
+                elif obfu.value == 'ollvm':
+                    for ob_pass in ollvm_pass:
+                        ob_pass_dir = obfu_dir / ob_pass.value
+                        ob_pass_dir.mkdir(parents=True, exist_ok=True)
+
 
     def download_plain(self, project: Project) -> None:
         # TODO: BARRE PROGRESSION
         # TODO: raise si quelconque exception
-        # TODO: récupérer lien .json
-        # TODO: requests
-        # TODO: unzip + rm du zip
-        pass
+
+        with open(self.json_links, 'r') as out:
+            json_data = json.load(out)
+            
+        project_sources = json_data[project.value]['source']
+        link = project_sources['link']
+        hash_value = project_sources['hash']
+        size = project_sources['size']
+        
+        r = requests.get(link)
+        data_path = Path(self.root_path) / project.value / 'data.zip'
+        open(data_path, 'wb').write(r.content)
+        
+        download_hash = hashlib.md5(data_path.open(mode='rb').read()).hexdigest()
+        if hash_value != download_hash: #Make sure the dowload was correct
+            logging.exception('The hash of the downloaded file is not the same as the registered one. Please try again')
+            exit()
+            
+        with ZipFile(data_path, 'r') as out:
+            out.extractall(path = data_path.parents[0] / "sources")
+        data_path.unlink()
+        
+
 
     def download_obfuscated(self, project: Project, obfuscator: Obfuscator, obfpass: ObPass) -> None:
         # TODO: BARRE PROGRESSION
         # TODO: raise si quelconque exception
-        # TODO: récupérer lien .json
-        # TODO: requests
-        # TODO: unzip + rm du zip
-        pass
 
+        with open(self.json_links, 'r') as out:
+            json_data = json.load(out)
+            
+        project_obfuscated = json_data[project.value]['obfuscated'][obfuscator][obfpass]
+        link = project_sources['link']
+        hash_value = project_sources['hash']
+        size = project_sources['size']
+        
+        r = requests.get(link)
+        data_path = Path(self.root_path) / project.value / 'data.zip'
+        open(data_path, 'wb').write(r.content)
+        
+        download_hash = hashlib.md5(data_path.open(mode='rb').read()).hexdigest()
+        if hash_value != download_hash: #Make sure the dowload was correct
+            logging.exception('The hash of the downloaded file is not the same as the registered one. Please try again')
+            exit()
+            
+        output_dir = data_path.parents[0] / "obfuscated" / obfuscator.value / obfpass.value
+        with ZipFile(data_path, 'r') as out:
+            out.extractall(path = output_dir)
+        data_path.unlink()
+        
+       
     def download_all(self) -> None:
         # TODO: BARRE PROGRESSION
-        # TODO: calculer sum des size du json
-        # TODO: Also download plain
         for proj in Project:
+            self.download_plain(proj)
             for obf in Obfuscator:
                 for obfpass in ObPass:
                     self.download_obfuscated(proj, obf, obfpass)
